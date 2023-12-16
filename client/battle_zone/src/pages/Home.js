dd4927575bec0b78b4ae97771e291338afa3d3f2
George <44335333+Georgi1102@users.noreply.github.com>
2023-12-16 19:00:02 +0200

    image_background_prettier-homepage

--- client/battle_zone/src/pages/Home.js
+++ client/battle_zone/src/pages/Home.js
@@ -1,21 +1,24 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import BestSellers from "../components/BestSellers";
+import "../style.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen">
       <NavigationBar></NavigationBar>
-      <div className=" bg-black pt-5 mb-8 text-center">
-        <h2 className="text-5xl text-white font-semibold">Welcome to BattleZone Bazaar</h2>
-        <p className="text-white text-2xl m-14 mb-10">
-          At BattleZone, we are passionate about providing high-quality
-          airsoft products for enthusiasts of all levels. Whether you're a
-          seasoned player or just starting, we have the gear you need to elevate
-          your airsoft experience.
+      <div className="flex flex-col items-start ml-12">
+        <h2 className="text-5xl text-white font-semibold ml-8 mb-4">
+          Welcome!
+        </h2>
+        <p className="text-white ml-8 text-left text-2xl max-w-md">
+          At BattleZone, we are passionate about providing high-quality airsoft
+          products for enthusiasts of all levels.
         </p>
       </div>
-      <BestSellers></BestSellers>
+      <div className="mt-36">
+        <BestSellers></BestSellers>
+      </div>
       <footer className="bg-black border-t-2 border-t-red-600 text-red p-4 mt-auto">
         <div className="container mx-auto text-center">
           <p>&copy; 2023 BattleZone Shop. All rights reserved.</p>
