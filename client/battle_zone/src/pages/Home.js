600150a7df4ddd2cf6d8bc467ddd2774925aca3d
George <44335333+Georgi1102@users.noreply.github.com>
2023-12-18 15:55:31 +0200

    home_image_opacity

--- client/battle_zone/src/pages/Home.js
+++ client/battle_zone/src/pages/Home.js
@@ -5,20 +5,20 @@ import "../style.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex opacity-90 flex-col min-h-screen">
       <NavigationBar></NavigationBar>
-      <div className="flex flex-col items-start ml-12">
-        <h2 className="text-5xl text-white font-semibold ml-8 mb-4">
-          Welcome!
-        </h2>
-        <p className="text-white ml-8 text-left text-2xl max-w-md">
+      <div className="flex flex-col items-start ml-12 mt-8">
+        <h2 className="text-5xl text-white font-semibold mb-4">Welcome!</h2>
+        <p className="text-white text-left text-2xl max-w-md">
           At BattleZone, we are passionate about providing high-quality airsoft
           products for enthusiasts of all levels.
         </p>
       </div>
-      <div className="mt-36">
-        <BestSellers></BestSellers>
+
+      <div className="mt-44">
+        <BestSellers />
       </div>
+
       <footer className="bg-black border-t-2 border-t-red-600 text-red p-4 mt-auto">
         <div className="container mx-auto text-center">
           <p>&copy; 2023 BattleZone Shop. All rights reserved.</p>
