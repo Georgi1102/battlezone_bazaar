d44ef6143901c75cbbd375c63dda54f394b5c7c3
anirdm <anirdm871@gmail.com>
2024-01-02 22:05:29 +0200

    Redesign Login page

--- client/battle_zone/src/pages/Login.jsx
+++ client/battle_zone/src/pages/Login.jsx
@@ -4,14 +4,19 @@ import "../style.css";
 export default function Login() {
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <div className="w-[30vw] h-[40vh] bg-black border-2 border-red-600 rounded-lg flex flex-col justify-evenly" >
-                <div className="text-red text-4xl flex justify-center ">
+            <div className="w-[30vw] h-[40vh] bg-dark-gray rounded-lg flex flex-col justify-evenly" >
+                <h1 className="text-white tracking-wider font-default-bold text-5xl flex justify-center ">
                     Login
-                </div>
-                <form className="h-44 flex flex-col items-center">
-                    <input className="w-[70%]" placeholder="Username..." maxLength={64} autoFocus={true}></input>
-                    <input className="w-[70%]" placeholder="Password..." maxLength={64} type="password"></input>
-                </form>
+                </h1>
+                <form className="h-50 space-y-5 flex flex-col items-center">
+                    <input className="w-[70%] rounded-md py-1.5 bg-white-transparent text-white" placeholder="Username" maxLength={64} autoFocus={true}></input>
+                    <input className="w-[70%] rounded-md py-1.5 bg-white-transparent text-white" placeholder="Password" maxLength={64} type="password"></input>
+                </form>   
+                <div className="flex flex-col items-center">
+                    <button className=" w-[35%] rounded-md text-white text-xl font-default-bold bg-green py-1 mt-3 flex justify-center">
+                        Log in
+                    </button>
+                </div>            
             </div>
         </div>
     );
